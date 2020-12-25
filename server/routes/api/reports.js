e5e5dff573ee76f85a0865efae6024436f4d825b
Gerrard Ezeugwa <ezeugwagerrard@gmail.com>
2020-12-25 23:46:22 +0100

    Updates

--- server/routes/api/reports.js
+++ server/routes/api/reports.js
@@ -11,6 +11,6 @@ reportRouter.post('/training', verifyToken, trim, reports.training);
 reportRouter.post('/activity', verifyToken, trim, reports.activity);
 reportRouter.post('/group', verifyToken, trim, reports.group);
 reportRouter.post('/freport', verifyToken, trim, reports.freport);
-reportRouter.get('/synodattendance', verifyToken, trim, reports.getSynodAttendance);
+reportRouter.post('/synodattendance', verifyToken, trim, reports.getSynodAttendance);
 
 export default reportRouter;
